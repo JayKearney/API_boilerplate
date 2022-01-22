ef50d5078d43c96ed57a181c383fc55626ce3102
JayKearney <indi.mh@gmail.com>
2022-01-22 19:32:01 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const token = config.API_KEY;
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+
+
+
+
